f3f209866a32d7954dce40c0832e9e549a4bc63e
Soni <sonisingh2004@gmail.com>
2026-06-09 11:14:30 +0530

    refactor: remove unused services, utility functions, components, and static assets from the frontend.

--- src/services/index.js
+++
@@ -1,7 +0,0 @@
-export { default as collegesService } from './colleges';
-export { default as scholarshipsService } from './scholarships';
-export { default as examsService } from './exams';
-export { default as coursesService } from './courses';
-export { default as blogsService } from './blogs';
-export { default as careersService } from './careers';
-export { default as reviewsService } from './reviews';
